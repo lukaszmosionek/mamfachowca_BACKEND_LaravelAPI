9337eb79037c6ad8bd86a05afb2793a9da528954
lukaszmosionek <mosioneklukasz@gmail.com>
2025-06-27 20:52:34 +0200

    add: Service Controller

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreServiceRequest;
+use App\Http\Requests\UpdateServiceRequest;
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+
+    public function index()
+    {
+        // Pokaż usługi zalogowanego usługodawcy
+        $services = auth()->user()->services()->get();
+        return response()->json($services);
+    }
+
+    public function store(StoreServiceRequest $request)
+    {
+        $service = auth()->user()->services()->create($request->validated());
+        return response()->json($service, 201);
+    }
+
+    public function show(Service $service)
+    {
+        $this->authorize('view', $service);
+        return response()->json($service);
+    }
+
+    public function update(UpdateServiceRequest $request, Service $service)
+    {
+        $this->authorize('update', $service);
+        $service->update($request->validated());
+        return response()->json($service);
+    }
+
+    public function destroy(Service $service)
+    {
+        $this->authorize('delete', $service);
+        $service->delete();
+        return response()->json(null, 204);
+    }
+}
