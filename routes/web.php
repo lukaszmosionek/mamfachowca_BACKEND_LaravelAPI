be227f08f5a37e526d0d6f713644bcaabe517407
lukaszmosionek <mosioneklukasz@gmail.com>
2025-07-02 11:26:23 +0200

    add: deploy script

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/deploy', function () {
+        // Path to the shell script
+        $scriptPath = storage_path('deploy.sh');
+        dump($scriptPath);
+        // $scriptPath = 'deploy.sh';
+
+        // Execute the script
+        $output = shell_exec("bash $scriptPath 2>&1");
+
+        // Log the output or do something with it
+        \Log::info($output);
+
+        dump($output);
+
+        // Return a response to the user
+        return response("Script executed successfully!");
 });
