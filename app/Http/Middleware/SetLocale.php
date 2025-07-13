0aceaa1e4b20b721ea92b86cf513de200c98c811
lukaszmosionek <mosioneklukasz@gmail.com>
2025-07-13 12:20:35 +0200

    fix: setLocale middleware

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -4,13 +4,29 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
 
 class SetLocale
 {
     public function handle($request, Closure $next)
     {
-        $locale = $request->header('Accept-Language', 'en');
-        App::setLocale($locale);
+        // Get all available locales from the resources/lang directory
+        $availableLocales = collect(File::directories(lang_path()))
+            ->map(function ($path) {
+                return basename($path);
+            })->toArray();
+
+        // Get the requested locale from the Accept-Language header
+        $locale = $request->header('Accept-Language', Config::get('app.locale'));
+        $locale = substr($locale, 0, 2); // Get first two characters (e.g. 'en' from 'en-US')
+
+        // Use requested locale if available, otherwise fallback to config('app.locale')
+        if (in_array($locale, $availableLocales)) {
+            App::setLocale($locale);
+        } else {
+            App::setLocale(Config::get('app.locale'));
+        }
 
         return $next($request);
     }
