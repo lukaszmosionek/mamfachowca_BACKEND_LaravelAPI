04ba80f203ee500b69e9e3580d151728bb53081d
lukaszmosionek <mosioneklukasz@gmail.com>
2025-07-17 21:25:25 +0200

    add: people you have chatted with

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -8,27 +8,60 @@ use App\Events\MessageSent;
 use App\Models\User;
 use App\Notifications\NewMessageNotification;
 use App\Notifications\NewNotification;
+use App\Traits\ApiResponse;
 use Illuminate\Support\Facades\Auth;
 
 class ChatController extends Controller
 {
+
+    use ApiResponse;
+
+    public function index(Request $request)
+    {
+        $authId = auth()->id(); // or however you get the auth user ID
+        $users = [];
+
+        $messages = Message::where('sender_id', $authId)->orWhere('receiver_id', $authId)
+                        ->orderBy('created_at', 'DESC')
+                        ->get(['sender_id', 'receiver_id']);
+
+        foreach ($messages as $message) {
+                if ($message->sender_id !== $authId) {
+                    $users[] = $message->sender_id;
+                } elseif ($message->receiver_id !== $authId) {
+                    $users[] = $message->receiver_id;
+                }
+        }
+
+        $users = array_unique($users);
+
+        $users = User::whereIn('id', $users)->get(['id', 'name', 'email']);
+
+        return $this->success($users, 'Users fetched successfully');
+    }
+
     // Get all messages between the authenticated user and the target user
-    public function index(User $user)
+    public function show($receiverId)
     {
         $authId = Auth::id();
+        $receiver = User::find($receiverId);
 
-        $messages = Message::where(function ($query) use ($authId, $user) {
+        if (!$receiver) {
+            return $this->error('Chat user not found', 404);
+        }
+
+        $messages = Message::where(function ($query) use ($authId, $receiver) {
             $query->where('sender_id', $authId)
-                  ->where('receiver_id', $user->id);
-        })->orWhere(function ($query) use ($authId, $user) {
-            $query->where('sender_id', $user->id)
+                  ->where('receiver_id', $receiver->id);
+        })->orWhere(function ($query) use ($authId, $receiver) {
+            $query->where('sender_id', $receiver->id)
                   ->where('receiver_id', $authId);
         })
         ->orderBy('created_at')->get();
 
         return response()->json([
             'messages' => $messages,
-            'receiver' => $user,
+            'receiver' => $receiver,
         ]);
     }
 
