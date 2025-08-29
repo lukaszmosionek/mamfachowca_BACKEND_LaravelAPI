4ab365d6d4bc650f1851cffcec0e4bd5bc4781dc
lukaszmosionek <mosioneklukasz@gmail.com>
2025-08-29 11:01:19 +0200

    test: add Service Show test

---
+++ tests/Unit/ServiceShowTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enum\Role;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Service;
+use App\Models\Language;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ServiceShowTest extends TestCase
+{
+    use RefreshDatabase;
+
+  public function test_it_fetches_a_service_by_id_successfully()
+    {
+        // Arrange
+        $provider = User::factory()->create(['role' => Role::PROVIDER]);
+        $service = Service::factory()
+            ->for($provider, 'provider')
+            ->hasPhotos(2)
+            ->create();
+
+        // Act
+        $response = $this->getJson(route('services.show', $service->id));
+
+        // Assert
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'success',
+                'message',
+                'data' => [
+                    'service' => [
+                        'id',
+                        'name',
+                        'provider' => [
+                            'id',
+                            'name',
+                            'availabilities'
+                        ],
+                        'photos',
+                        'translations'
+                    ]
+                ]
+            ])
+            ->assertJsonFragment([
+                'message' => 'Service fetched successfully'
+            ]);
+    }
+
+   public function test_it_returns_404_if_service_not_found()
+    {
+        $response = $this->getJson(route('services.show', 99999));
+
+        $response->assertStatus(404);
+    }
+}
