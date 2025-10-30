9664dd30b01ee8d2a848c6ff206f29def89c10cb
lukaszmosionek <mosioneklukasz@gmail.com>
2025-10-30 17:25:09 +0100

    fix: update CORS allowed origins to use correct domain

--- config/cors.php
+++ config/cors.php
@@ -18,10 +18,10 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://mamfachowca.mosioneklukasz.pl'),
-        env('FRONTEND_URL_2', 'http://www.mamfachowca.mosioneklukasz.pl'),
-        env('FRONTEND_URL_3', 'https://mamfachowca.mosioneklukasz.pl'),
-        env('FRONTEND_URL_4', 'https://www.mamfachowca.mosioneklukasz.pl'),
+        env('FRONTEND_URL', 'http://mamfachowca.pl'),
+        env('FRONTEND_URL_2', 'http://www.mamfachowca.pl'),
+        env('FRONTEND_URL_3', 'https://mamfachowca.pl'),
+        env('FRONTEND_URL_4', 'https://www.mamfachowca.pl'),
         env('FRONTEND_URL_5'),
         env('FRONTEND_URL_6'),
     ], // port Vue
